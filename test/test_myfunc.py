c9d8f6a0e9d72ef005048a762d5cfffb456eb8e6
ivis-saito <makio.saito@ivis.co.jp>
2023-11-10 10:51:13 +0900

    test

--- test/test_myfunc.py
+++ test/test_myfunc.py
@@ -7,11 +7,3 @@ def test_add():
 
 def test_sub():
     assert sub(3, 1) == 2
-
-
-class TestCase:
-    def test_true(self):
-        assert True
-
-    def test_add_zero(self):
-        assert add(1, 0) == 1
