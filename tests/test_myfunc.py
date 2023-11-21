71aeef73f420fd9fadea3bc659bfffca9b48f20d
ivis-saito <makio.saito@ivis.co.jp>
2023-11-21 14:13:43 +0900

    失敗

--- tests/test_myfunc.py
+++ tests/test_myfunc.py
@@ -6,7 +6,7 @@ def test_add():
 
 
 def test_sub():
-    assert sub(2, 1) == 1
+    assert sub(2, 1) == 2
 
 
 class TestCase:
@@ -14,7 +14,7 @@ class TestCase:
         assert True
 
     def test_add_zero(self):
-        assert add(1, 0) == 1
+        assert add(1, 0) == 0
 
     def test_sub_zero(self):
         assert sub(1, 0) == 1
